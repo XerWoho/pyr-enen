453c41b30c21edf846aecfa5788ca5b484ed41a8
XerWo <realgamerex@gmail.com>
2025-07-01 01:30:23 +0200

    changes

--- js/bg.js
+++ js/bg.js
@@ -5,10 +5,10 @@ for(let i = 0; i < 30; i++) {
 	img.src = `assets/tree_${Math.floor(Math.random() * 3)}.svg`
 
 	let margin_top = Math.floor(Math.random() * document.body.clientHeight / 1.3)
-	if(margin_top > 1500) {
-		margin_top = 1500
+	if(margin_top > 3000) {
+		margin_top = 3000
 	}
-	img.style.marginTop = `${Math.floor(Math.random() * document.body.clientHeight / 1.3)}px`
+	img.style.marginTop = `${margin_top}px`
 	img.style.marginLeft = `${Math.floor(Math.random() * document.body.clientWidth / 1)}px`
 
 	let min = 3
